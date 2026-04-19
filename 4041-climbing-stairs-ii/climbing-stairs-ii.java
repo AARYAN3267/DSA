d2278de442fef508ac4bfbab3bbc3a75f7de44a6
AARYAN3267 <aaryantiwari1210@gmail.com>
2026-04-20 00:08:54 +0530

    Time: 7 ms (71.68%) | Memory: 107.7 MB (99.12%) - LeetSync

---
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    int arr[];
+    public int cost(int i, int j){
+         return arr[j]+ (j-i)*(j-i);
+    }
+    public int climbStairs(int n, int[] costs) {
+int dp[]= new int [n+1];
+arr= new int[n+1];
+for(int i=0; i< n; i++){
+    arr[i+1]= costs[i];
+}
+dp[0]=0;
+dp[1]=cost(0,1);
+if(n==1)return dp[1];
+dp[2]=Math.min(dp[1]+cost(1,2),cost(0,2));
+for(int i=3 ;i<=n;i++){
+    int x= cost(i-1,i);
+    int y = cost(i-2,i);
+    int z= cost(i-3,i);
+    dp[i]= Math.min(dp[i-1]+x,Math.min(dp[i-2]+y,dp[i-3]+z));
+}
+return dp[n];
+    }
+}
