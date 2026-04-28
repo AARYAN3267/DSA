4c6dab1a8f252dcbb4285d66bec91dc780cdfd2a
AARYAN3267 <aaryantiwari1210@gmail.com>
2026-04-28 21:50:01 +0530

    Time: 8 ms (100.00%) | Memory: 108.1 MB (77.86%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,20 +1,32 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
-      ArrayList<Integer>list = new ArrayList<>();
-        for(int[]arr:grid){
-            for(int a: arr){
-                list.add(a);
+        int n = grid.length, m = grid[0].length;
+        int[] freq = new int[10001];
+        //check -1 cases
+        boolean foundFalse = false;
+        int currChoice = grid[0][0];
+        for (int[] row : grid) {
+            for (int y : row) {
+                freq[y]++;
+                if (Math.abs(currChoice - y) % x != 0)
+                    foundFalse = true;
             }
         }
-       Collections.sort(list);
-       int mid= list.get(list.size()/2);
-       int ans=0;
-       for(int a: list){
-        if((a-mid)%x!=0)return -1;
-        else{
-        ans+= Math.abs((a-mid)/x);
+        if (foundFalse) return -1;
+        //sorting
+        int[] arr = new int[n * m];
+        int k = 0;
+        for (int i = 0; i < 10001; i++) {
+            for (int j = 0; j < freq[i]; j++) {
+                arr[k++] = i;
+            }
         }
-       }
- return ans;
+        int count = 0;
+        //pick best middle element
+        int median = arr[arr.length / 2];
+        for (int num : arr) {
+            count += Math.abs(num - median) / x;
+        }
+        return count;
     }
 }
