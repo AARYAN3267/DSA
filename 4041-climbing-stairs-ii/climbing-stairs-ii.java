1b03814dcead5fbb78aa73b682d316e07f6e7c8a
AARYAN3267 <aaryantiwari1210@gmail.com>
2026-08-06 03:44:26 +0530

    Time: 43 ms (17.14%) | Memory: 131.8 MB (9.47%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,24 +1,24 @@
 class Solution {
-    int arr[];
-    public int cost(int i, int j){
-         return arr[j]+ (j-i)*(j-i);
-    }
     public int climbStairs(int n, int[] costs) {
-int dp[]= new int [n+1];
-arr= new int[n+1];
-for(int i=0; i< n; i++){
-    arr[i+1]= costs[i];
-}
-dp[0]=0;
-dp[1]=cost(0,1);
-if(n==1)return dp[1];
-dp[2]=Math.min(dp[1]+cost(1,2),cost(0,2));
-for(int i=3 ;i<=n;i++){
-    int x= cost(i-1,i);
-    int y = cost(i-2,i);
-    int z= cost(i-3,i);
-    dp[i]= Math.min(dp[i-1]+x,Math.min(dp[i-2]+y,dp[i-3]+z));
-}
-return dp[n];
+     int dp[]= new int[n+1];
+     Arrays.fill(dp,-1);
+     return min(n,dp,costs);}
+    public int min(int n, int dp[], int cost[]){
+        int ans= Integer.MAX_VALUE;
+   if(n==0)return 0;
+   if(dp[n]!=-1)return dp[n];
+   if(n>=1){
+ ans =Math.min(ans, min(n-1,dp,cost)+costcal(n,1,cost));
+   }
+    if(n>=2){
+ ans =Math.min(ans, min(n-2,dp,cost)+costcal(n,2,cost));
+   }
+    if(n>=3){
+ ans =Math.min(ans, min(n-3,dp,cost)+costcal(n,3,cost));
+   }
+   return dp[n]=ans;
+    }
+    public int costcal(int i, int j ,int cost[]){
+        return cost[i-1]+j*j;
     }
 }
