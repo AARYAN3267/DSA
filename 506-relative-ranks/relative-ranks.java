61f864fdf94fa19b0573f695390c52236ece06dd
AARYAN3267 <aaryantiwari1210@gmail.com>
2026-08-18 01:40:10 +0530

    Time: 9 ms (86.36%) | Memory: 47.2 MB (92.92%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+     int n = score.length;
+        String[] result = new String[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
+        
+        for (int i = 0; i < n; i++) {
+            pq.offer(new int[]{score[i], i});
+        }
+        
+        int rank = 1;
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+            int index = current[1];
+            
+            if (rank == 1) {
+                result[index] = "Gold Medal";
+            } else if (rank == 2) {
+                result[index] = "Silver Medal";
+            } else if (rank == 3) {
+                result[index] = "Bronze Medal";
+            } else {
+                result[index] = ""+rank;
+            }
+            rank++;
+        }
+        
+        return result;
+    }
+}
