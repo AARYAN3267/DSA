57a27965d66b9c4fbb1978bc7893cbcff650692f
AARYAN3267 <aaryantiwari1210@gmail.com>
2026-09-03 19:07:48 +0530

    Time: 6 ms (56.52%) | Memory: 121.7 MB (62.32%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int small=Integer.MAX_VALUE;
+      for(int i:nums1){
+     small= Math.min(i,small);
+      }
+
+      if(small%2==0){
+        for(int a:nums1){
+            if(a%2!=0)return false;
+        }
+      }
+      else{
+        for(int a:nums1){
+            if(a%2==0&&(a-small)%2==0)return false;
+        }
+      }  
+      return true;
+    }
+}
