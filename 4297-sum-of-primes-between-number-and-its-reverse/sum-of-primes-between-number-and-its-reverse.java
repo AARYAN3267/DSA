3799aec91ac80d708a8c432afed7c28c2499fedc
AARYAN3267 <aaryantiwari1210@gmail.com>
2026-09-05 23:15:32 +0530

    Time: 3 ms (96.48%) | Memory: 43 MB (14.81%) - LeetSync

--- 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.java
+++ 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.java
@@ -8,21 +8,34 @@ class Solution {
         }
         return a;
     }
-    private boolean isPrime(int n){
-        for(int i=2;i<=Math.sqrt(n);i++){
-            if(n%i==0)return false;
-        }
-        return true;
-    }
+    // private boolean isPrime(int n){
+    //     for(int i=2;i<=Math.sqrt(n);i++){
+    //         if(n%i==0)return false;
+    //     }
+    //     return true;
+    // }
     public int sumOfPrimesInRange(int n) {
       int rev= reverse(n);
       int min= Math.min(rev,n);
       int sum=0;
       int max= Math.max(rev,n);
-      for(int i=min;i<=max;i++){
-        if(i==1)continue;
-        if(isPrime(i))sum+=i;
-      }
+    //   for(int i=min;i<=max;i++){
+    //     if(i==1)continue;
+    //     if(isPrime(i))sum+=i;
+    //   }
+
+    //using sieve algo coz its more optimized
+    boolean arr[]= new boolean[max+1];
+    for(int i=2;i*i<=max;i++){
+        for(int j=i*i;j<=max;j+=i){
+            arr[j]=true;
+        }
+    }
+    arr[0]= true;
+    arr[1]=true;
+    for(int i=min;i<=max;i++){
+        if(!arr[i])sum+=i;
+    }
       return sum;
     }
 }
