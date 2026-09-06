76d096301e34158c970516cf49c89645e8c3fd53
AARYAN3267 <aaryantiwari1210@gmail.com>
2026-09-06 15:23:22 +0530

    Time: 20 ms (53.25%) | Memory: 54.4 MB (50.85%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,18 +1,41 @@
 class Solution {
-    private int fun(String s, String t, int m, int n,Integer dp[][]){
- if(n==0)return dp[m][n]= 1;
- if (m==0)return dp[m][n]= 0;
- if(dp[m][n]!=null)return dp[m][n];
-        if(s.charAt(m-1)==t.charAt(n-1)){
-     return dp[m][n]=fun(s,t,m-1,n-1,dp)+fun(s,t,m-1,n,dp);     
-    }
- return dp[m][n]= fun(s,t,m-1,n,dp);
-    }
-    public int numDistinct(String s, String t) {
+//     private int fun(String s, String t, int m, int n,Integer dp[][]){
+//  if(n==0)return dp[m][n]= 1;
+//  if (m==0)return dp[m][n]= 0;
+//  if(dp[m][n]!=null)return dp[m][n];
+//         if(s.charAt(m-1)==t.charAt(n-1)){//if matchedd either pick it or not pick it for further searching same char after it;
+//      return dp[m][n]=fun(s,t,m-1,n-1,dp)+fun(s,t,m-1,n,dp); 
+//     }
+//  return dp[m][n]= fun(s,t,m-1,n,dp);//not matched so no other option find in future only;
+//     }
+//     public int numDistinct(String s, String t) {
     
-      int m= s.length();
-      int n=t.length();
-      Integer dp[][]= new Integer[m+1][n+1];
-      return fun(s,t,m,n,dp);  
+//       int m= s.length();
+//       int n=t.length();
+//       Integer dp[][]= new Integer[m+1][n+1];
+//       return fun(s,t,m,n,dp);  
+//     }
+
+public int numDistinct(String s, String t){
+    int m = s.length();
+    int n= t.length();
+    int dp[][]= new int [m+1][n+1];
+    for(int i=0; i<=m;i++){
+        dp[i][0]=1;
     }
+    for(int i=1;i<=n;i++){
+        dp[0][i]=0;
+    }
+    for(int i=1; i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s.charAt(i-1)==t.charAt(j-1)){
+                dp[i][j]= dp[i-1][j-1]+dp[i-1][j];
+            }
+            else{
+                dp[i][j]= dp[i-1][j];
+            }
+        }
+    }
+    return dp[m][n];
+}
 }
