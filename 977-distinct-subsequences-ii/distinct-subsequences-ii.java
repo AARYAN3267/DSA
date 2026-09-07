604380258550d416ad0b6ee952db48ec5bea48ba
AARYAN3267 <aaryantiwari1210@gmail.com>
2026-09-07 20:30:28 +0530

    Time: 4 ms (70.57%) | Memory: 43.8 MB (25.89%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.java
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -25,30 +25,57 @@ class Solution {
 //        StringBuilder sb= new StringBuilder();
 //        return ((fun(s,sb,set,0)-1)+mod)%mod; 
 //     }
- private int fun(int n,int []prev,Integer[]dp){
-  int mod=1_000_000_007;
-  if(n==0)return 1;
-  if(dp[n]!=null)return dp[n];
 
-    int total=(2*fun(n-1,prev,dp))%mod;
-    if(prev[n]!=0){
-        int duplicate=fun(prev[n]-1,prev,dp);
-        total= ((total-duplicate)+mod)%mod;
-    }
-    return dp[n]=total;
-    }
-    public int distinctSubseqII(String s) {
-        Integer dp[]= new Integer[2001];
+//IF A CHARACTER APPEAR ONCE AGAIN DELETE THE NO OF SUBSEQUENCE IT HAD PREVIOUSLY MADE 
+// WE CAN CALCULATE TOTAL NO OF SUBSEQUENCE BY 2^N;
+
+//  private int fun(int n,int []prev,Integer[]dp){
+//   int mod=1_000_000_007;
+//   if(n==0)return 1;
+//   if(dp[n]!=null)return dp[n];
+
+//     int total=(2*fun(n-1,prev,dp))%mod;
+//     if(prev[n]!=0){
+//         int duplicate=fun(prev[n]-1,prev,dp);
+//         total= ((total-duplicate)+mod)%mod;
+//     }
+//     return dp[n]=total;
+//     }
+//     public int distinctSubseqII(String s) {
+//         Integer dp[]= new Integer[2001];
+//   int mod=1_000_000_007;
+//   int n= s.length();
+//       int lastseen[]= new int[27];
+//       int prev[]=new int[n+1];
+//       for(int i=1;i<=n;i++){
+//         int ind= s.charAt(i-1)-'a';
+//         prev[i]=lastseen[ind];
+//         lastseen[ind]=i;
+//       }
+//       return ((fun(n,prev,dp)-1)+mod)%mod;
+       
+//     }
+    public int distinctSubseqII(String s){
+        int dp[]= new int[2001];
   int mod=1_000_000_007;
   int n= s.length();
-      int lastseen[]= new int[27];
-      int prev[]=new int[n+1];
-      for(int i=1;i<=n;i++){
+  int prev[]=new int[n+1];
+  dp[0]=1;
+  int lastseen[]= new int[27];
+   for(int i=1;i<=n;i++){
         int ind= s.charAt(i-1)-'a';
         prev[i]=lastseen[ind];
         lastseen[ind]=i;
       }
-      return ((fun(n,prev,dp)-1)+mod)%mod;
-       
+  for(int i=1; i<=n;i++){
+    int total=(2*dp[i-1])%mod;
+  
+    if(prev[i]!=0){
+        int duplicate= dp[prev[i]-1];
+        total= ((total-duplicate)+mod)%mod;
+    }
+    dp[i]=total;
+  } 
+  return (dp[n]-1+mod)%mod;
     }
 }
