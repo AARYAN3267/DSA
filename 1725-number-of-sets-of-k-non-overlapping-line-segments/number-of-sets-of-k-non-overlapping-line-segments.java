e8e806faaf22ce94747a07194b954ecb903587ac
AARYAN3267 <aaryantiwari1210@gmail.com>
2026-09-16 21:06:35 +0530

    Time: 208 ms (16.77%) | Memory: 167.6 MB (5.81%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -5,13 +5,15 @@ long ways=0;
 if(k==0)return dp[i][k][conti]=1;
 if(i==n)return dp[i][k][conti]=0;
 if(dp[i][k][conti]!=null)return dp[i][k][conti];
+//if we are continuing to draw the segment
 if(conti==1){
- ways= (ways+fun(n,i+1,k,1,dp))%mod;
-  ways= (ways+ fun(n,i,k-1,0,dp))%mod;
+ ways= (ways+fun(n,i+1,k,1,dp))%mod;//either we can continue to take
+  ways= (ways+ fun(n,i,k-1,0,dp))%mod;//or we can stop
 }
 else{
-ways= (ways+fun(n,i+1,k,1,dp))%mod;
-ways= (ways+fun(n,i+1,k,0,dp))%mod;
+    //if we have to start another segement
+ways= (ways+fun(n,i+1,k,1,dp))%mod;//we can take this posi
+ways= (ways+fun(n,i+1,k,0,dp))%mod;//or we can skip this posi
 }
 return dp[i][k][conti]=(int)ways;
     }
